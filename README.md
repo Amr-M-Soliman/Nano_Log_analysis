b6a5b732b80265e8d49b8c681bcc1c22509562ef
Amr M.Soliman <46072869+Amr-M-Soliman@users.noreply.github.com>
2018-12-23 00:04:13 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
-## Check View ##
- Check if a view named **"top_articles"** is excisted or not.
-```
-SELECT EXISTS (SELECT 1 FROM information_schema.Views WHERE table_name = 'top_articles');
-```           
+## Setup and run the code ##
+The code is written in **python3**.to run it use **CMD** 
+
+ `python news_query.py`
+    
 ## Create a View ##
-Create a view named **'top_articles'** Includes (**author_id ,articles_id ,articles_title and `count(articles.slug))`**.
+You may create a view named **'top_articles'** Includes (**author_id ,articles_id ,articles_title and `count(articles.slug))`**.
 Method `[count(articles.slug)]` is used for counting all views of all articles written by each authour.
 ```
 CREATE VIEW top_articles AS 
@@ -18,6 +18,11 @@ CREATE VIEW top_articles AS
         GROUP BY 1,2,3
         ORDER BY 4 DESC;
 ```
+## Check View ##
+ Check if a view named **"top_articles"** is excisted or not.
+```
+SELECT EXISTS (SELECT 1 FROM information_schema.Views WHERE table_name = 'top_articles');
+```           
 ## First Query: What are the most popular three articles of all time? ##
 Use the View **top_articles** to get the 3 articles
 ```
@@ -26,7 +31,7 @@ FROM top_articles
 LIMIT 3;
  ```
     
-## Second Query: Who are the most popular article authors of all time?
+## Second Query: Who are the most popular article authors of all time? ##
 ```
 SELECT u.name AS author,SUM(ta.views) AS views 
 FROM authors u 
@@ -39,14 +44,14 @@ ORDER BY 2 DESC;
 Creat a **subquery** that counts the errors in log table for each day.Errors are the status rows that are not like ** '200 OK'. **
 ```
 SELECT DATE_TRUNC('day',time)AS err_time ,COUNT(status) AS err_sum 
-	    FROM log 
+			FROM log 
             WHERE status !='200 OK' 
-	    GROUP BY 1
+			GROUP BY 1
 ```
 Then  create a second subquery that count all status rows in log table for each day.
 ```
 SELECT DATE_TRUNC('day',time)AS req_time ,COUNT(status) AS req_sum 
-	    FROM log 
+			FROM log 
             GROUP BY 1
 ```
 By using those two queries,you can get the days which more than 1% of requests lead to errors.
@@ -54,12 +59,12 @@ Using this method `ROUND((err.err_sum*1.0 / req.req_sum)*100,2)` to get the erro
 ```
 WITH 
 	err AS (SELECT DATE_TRUNC('day',time)AS err_time,COUNT(status) AS err_sum 
-	        FROM log 
-            	WHERE status !='200 OK' 
-	    	GROUP BY 1 ) ,
+			FROM log 
+            WHERE status !='200 OK' 
+			GROUP BY 1 ) ,
 	req AS (SELECT DATE_TRUNC('day',time)AS req_time ,COUNT(status) AS req_sum 
-		FROM log 
-            	GROUP BY 1  )  
+			FROM log 
+            GROUP BY 1  )  
 SELECT req_time,ROUND((err.err_sum*1.0 / req.req_sum)*100,2) AS errors_percentage
 FROM req
 JOIN err
--- news_query.py
+++ news_query.py
@@ -1,49 +1,83 @@
+#!/usr/bin/env python3
 import psycopg2 as psycopg2
 
 DBNAME = 'news'
 NAME_PASS = 'postgres'
 
-check_view = "SELECT EXISTS (SELECT 1 FROM information_schema.Views WHERE table_name = 'top_articles');"
+check_view = "SELECT EXISTS (SELECT 1 FROM information_schema.Views" \
+             " WHERE table_name = 'top_articles');"
 
-new_view = "CREATE VIEW top_articles AS SELECT authors.id AS author_id ,articles.id AS article_id ,articles.title AS " \
-           "article_name,count(articles.slug) AS views FROM articles JOIN log " \
+new_view = "CREATE VIEW top_articles AS SELECT authors.id AS author_id," \
+           "articles.id AS article_id ,articles.title AS " \
+           "article_name,count(articles.slug) AS views " \
+           "FROM articles JOIN log " \
            "ON ('/article/'::text || articles.slug) = log.path " \
-           "JOIN authors on  articles.author = authors.id GROUP BY 1,2,3 " \
+           "JOIN authors on  articles.author = authors.id " \
+           "GROUP BY 1,2,3 " \
            "ORDER BY 4 DESC ;"
 
 first_query = "SELECT article_name,views FROM top_articles LIMIT 3;"
 
-second_query = "SELECT u.name AS author ,SUM(ta.views) AS views FROM authors u JOIN top_articles ta " \
+second_query = "SELECT u.name AS author ,SUM(ta.views) AS views " \
+               "FROM authors u JOIN top_articles ta " \
                "ON u.id = ta.author_id GROUP BY 1 ORDER BY 2 DESC;"
 
-third_query = "WITH err AS (SELECT DATE_TRUNC('day',time)AS err_time ,COUNT(status) AS err_sum FROM log " \
-              "WHERE status !='200 OK' GROUP BY 1 ),req AS (SELECT DATE_TRUNC('day',time)AS req_time ," \
-              "COUNT(status) AS req_sum FROM log GROUP BY 1)SELECT req_time," \
-              "ROUND((err.err_sum*1.0 / req.req_sum)*100,2) AS errors_percentage FROM req JOIN err " \
-              "ON err_time = req_time GROUP BY 1,2 HAVING ROUND((err.err_sum*1.0 / req.req_sum)*100,2) > 1 "
+third_query = "WITH err AS (SELECT DATE_TRUNC('day',time)AS err_time," \
+              "COUNT(status) AS err_sum FROM log " \
+              "WHERE status !='200 OK' GROUP BY 1 )," \
+              "req AS (SELECT DATE_TRUNC('day',time)AS req_time ," \
+              "COUNT(status) AS req_sum FROM log GROUP BY 1)" \
+              "SELECT req_time," \
+              "ROUND((err.err_sum*1.0 / req.req_sum)*100,2) " \
+              "AS errors_percentage " \
+              "FROM req JOIN err " \
+              "ON err_time = req_time GROUP BY 1,2 " \
+              "HAVING ROUND((err.err_sum*1.0 / req.req_sum)*100,2) > 1 "
 
 
 def create_view():
-    db = psycopg2.connect(database=DBNAME, user=NAME_PASS, password=NAME_PASS)
-    c = db.cursor()
-    c.execute(check_view)
-    view_bool = c.fetchone()[0]
-    if not view_bool:
-        c.execute(new_view)
-        db.commit()
-    db.close()
+    try:
+        # Connect to news database
+        db = psycopg2.connect(database=DBNAME, user=NAME_PASS,
+                              password=NAME_PASS)
+        c = db.cursor()
+        # First you check the view named'top_articles'
+        c.execute(check_view)
+        view_bool = c.fetchone()[0]
+        # If the view is not existed
+        if not view_bool:
+            # Create a new view named'top_articles'
+            c.execute(new_view)
+            # add the view to news database
+            db.commit()
+        # close connection
+        db.close()
+    except (Exception, psycopg2.Error) as error:
+        print("Error while connecting to PostgreSQL using get_query()", error)
 
 
 def get_query(q):
-    db = psycopg2.connect(database=DBNAME, user=NAME_PASS, password=NAME_PASS)
-    create_view()
-    c = db.cursor()
-    c.execute(q)
-    data = c.fetchall()
-    db.close()
-    return data
+    try:
+        # Connect to news database
+        db = psycopg2.connect(database=DBNAME, user=NAME_PASS,
+                              password=NAME_PASS)
+        # Check the view table named'top_articles'
+        create_view()
+        c = db.cursor()
+        # execute the query
+        c.execute(q)
+        data = c.fetchall()
+        # close connection
+        db.close()
+        # return query result
+        return data
+    except (Exception, psycopg2.Error) as error:
+        print("Error while connecting to PostgreSQL using get_query()", error)
 
 
+# First Query: What are the most popular three articles of all time?
 print("First_Query: " + str(get_query(first_query)))
+# Second Query: Who are the most popular article authors of all time?
 print("Second_Query: " + str(get_query(second_query)))
+# Third Query: Who are the most popular article authors of all time?
 print("Third_Query: " + str(get_query(third_query)))
